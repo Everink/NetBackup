8912e22daffa90ca84f5adffbabbdd38e503e5b6
Roel Everink <roel@everink.it>
2018-09-25 14:59:35 +0200

    connect-nbserver parameters

--- NetBackup.psm1
+++ NetBackup.psm1
@@ -2,20 +2,47 @@
 function Connect-NBServer {
    [CmdletBinding()]
    param (
-      [string]$Server,
-      [int]$Port = 1553
+      [parameter(Mandatory = $true)]
+      [ValidateNotNullOrEmpty()]
+      [string]$Server = "localhost",
+      
+      [parameter(Mandatory = $true, ParameterSetName = "Username")]
+      [ValidateNotNullOrEmpty()]
+      [String]$Username,
+
+      [parameter(Mandatory = $true, ParameterSetName = "Username")]
+      [ValidateNotNullOrEmpty()]
+      [SecureString]$Password,
+
+      [Parameter(Mandatory = $true, ParameterSetName = "Credential")]
+      [ValidateNotNullOrEmpty()]
+      [Management.Automation.PSCredential]$Credential,
+
+      [int]$Port = 1556
    )
    
-   begin {
-      Get-Service
+   if ($PSCmdlet.ParameterSetName -eq "Credential") {
+      $Username = $Credential.UserName
+      $Password = $Credential.GetNetworkCredential().Password
    }
+
+   $Uri = "https://$($Server):$Port/netbackup/login"
+
+   $Headers = @{
+      "content-type" = "application/vnd.netbackup+json;version=1.0"
+   }
+
+   $Body = @{
+      #"domainType" = "vx"                                     
+      #"domainName" = "mydomain"                               
+      "userName"   = $Username                               
+      "password"   = $Password
+   } | ConvertTo-Json 
    
-   process {
-      
-   }
    
-   end {
-   }
+   Invoke-RestMethod -Method POST -Uri $Uri -Headers $Headers -Body $Body
+   
+
 }
 
 
