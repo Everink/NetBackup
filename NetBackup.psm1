106a9f8b81b4af0587838135aaed380d395186ca
Roel Everink <roel@everink.it>
2018-09-25 10:59:40 +0200

    New file

---
+++ NetBackup.psm1
@@ -0,0 +1,20 @@
+
+function Connect-NBServer {
+   [CmdletBinding()]
+   param (
+      [string]$Server,
+      [int]$Port = 1553
+   )
+   
+   begin {
+   }
+   
+   process {
+      
+   }
+   
+   end {
+   }
+}
+
+
